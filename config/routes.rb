5bdb268c9cfb33cd8a9f715f5896f17d582478f6
KimieJoe <kimiejoeart@gmail.com>
2018-04-24 09:54:09 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # get 'storefront/all_items'
+  #
+  # get 'storefront/items_by_category'
+  #
+  # get 'storefront/items_by_brand'
+
+  root 'storefront#all_items'
+  
+  get 'categorical' => 'storefront#items_by_category'
+  
+  get 'branding' => 'storefront#items_by_brand'
+
+  resources :categories
+  resources :products
+  
+  # root 'products#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
